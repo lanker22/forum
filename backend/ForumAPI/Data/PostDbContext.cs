574d5f8b0184f73a96a18adb9f3b1248c3be137f
George Everett <gjemba@hotmail.com>
2020-12-16 23:31:40 +0000

    Add database context classes

---
+++ backend/ForumAPI/Data/PostDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ForumAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumAPI.Data
+{
+    public class PostDbContext : DbContext 
+    {
+        public PostDbContext(DbContextOptions<PostDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ backend/ForumAPI/Data/ThreadDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ForumAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumAPI.Data
+{
+    public class ThreadDbContext : DbContext
+    {
+        public ThreadDbContext(DbContextOptions<ThreadDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Thread> Threads { get; set; }
+    }
+}
