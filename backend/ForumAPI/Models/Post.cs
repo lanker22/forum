0612c00aee5928e1fd21ec346dde7a6463086f85
George Everett <gjemba@hotmail.com>
2020-12-16 23:31:23 +0000

    create models

---
+++ backend/ForumAPI/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumAPI.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public ICollection<Post> Replies { get; set; }
+        public User User { get; set; }
+        
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+    }
+}
---
+++ backend/ForumAPI/Models/Thread.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumAPI.Models
+{
+    public class Thread
+    {
+        [Key]
+        public int ThreadId { get; set; }
+        public string Title { get; set; }
+        public ICollection<Post> Posts { get; set; }
+        public bool IsSticky { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey("SubForum")]
+        public int SubForumId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+    }
+}
