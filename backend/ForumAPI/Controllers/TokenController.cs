72f532e0d93ed41f8ddefa0f7cbd5b0aa7d92a40
George Everett <gjemba@hotmail.com>
2020-12-17 23:59:53 +0000

    jwt token controller

---
+++ backend/ForumAPI/Controllers/TokenController.cs
@@ -0,0 +1,33 @@
+﻿using ForumAPI.Data;
+using ForumAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumAPI.Controllers
+{
+    public class TokenController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public TokenController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Route("/token")]
+        [HttpPost]
+        public async Task<IActionResult> Create(string grant_type, )
+        {
+            
+        }
+
+        public async Task<bool> IsValidUsernameAndPassword()
+    }
+}
