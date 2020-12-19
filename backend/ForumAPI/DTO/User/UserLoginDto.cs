79324a98fd557b543828ee441529b9b74e98e881
George Everett <gjemba@hotmail.com>
2020-12-19 16:26:45 +0000

    Add data-transfer-objects

---
+++ backend/ForumAPI/DTO/User/UserLoginDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumAPI.DTO.User
+{
+    public class UserLoginDto
+    {
+        [ProtectedPersonalData]
+        [Required]
+        public string Username { get; set; }
+
+        [ProtectedPersonalData]
+        [Required]
+        [DataType("Password")]
+        public string Password { get; set; }
+    }
+}
